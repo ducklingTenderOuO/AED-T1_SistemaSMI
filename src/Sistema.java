5923c28e81ba3311aab4712ae1a249135bd0f1b8
Raúl García <158521073+Alionsan@users.noreply.github.com>
2026-08-18 09:34:16 -0700

    Add files via upload

--- src/Sistema.java
+++ src/Sistema.java
@@ -1,9 +1,7 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Sistema {
-    ArrayList<Instrumento> inventario = new ArrayList<>();
-    Instrumento [] losInstrumentos = new Instrumento[10];
+    Instrumento[] inventario = new Instrumento[10];
     int contador = 0;
     Scanner leer = new Scanner(System.in);
 
@@ -41,17 +39,9 @@ public class Sistema {
     }
 
     public void registrarInstrumento() {
-        try {
-            if (contador >= 0 && contador < losInstrumentos.length){
-                System.out.println("Espacio para instrumento.");
-            }else{
-                System.out.println("Fuera de rango.");
-            }
-        }catch (Exception e){
-            System.out.println("Error.");
+        if (contador >= inventario.length){
+            aumentarCapacidad();
         }
-
-        losInstrumentos[contador] = new Instrumento();
 
         System.out.println("\nRegistrar Instrumento");
 
@@ -84,10 +74,12 @@ public class Sistema {
         }
 
         Instrumento nuevo = new Instrumento(clave, nombre, forma, tipo, condicion, autor, cita, evaluacion);
-        inventario.add(nuevo);
+        inventario[contador] = nuevo;
+        contador++;
         System.out.println("Instrumento registrado con éxito");
 
     }
+
     public void menuConsultas() {
         int opcionConsulta = 0;
         do {
@@ -97,57 +89,89 @@ public class Sistema {
             System.out.println("3. Regresar al menú principal");
             System.out.print("Elige una opción: ");
 
-            opcionConsulta = Integer.parseInt(leer.nextLine());
+            try {
+                opcionConsulta = Integer.parseInt(leer.nextLine());
 
-            switch (opcionConsulta) {
-                case 1:
-                    consultarPorAutor();
-                    break;
-                case 2:
-                    consultarPorTipo();
-                    break;
-                case 3:
-                    System.out.println("Regresando al menú principal.");
-                    break;
-                default:
-                    System.out.println("Opción no válida.");
+                switch (opcionConsulta) {
+                    case 1:
+                        consultarPorAutor();
+                        break;
+                    case 2:
+                        consultarPorTipo();
+                        break;
+                    case 3:
+                        System.out.println("Regresando al menú principal.");
+                        break;
+                    default:
+                        System.out.println("Opción no válida.");
+                }
+            } catch (NumberFormatException e) {
+                System.out.println("Ingresa un número válido.");
             }
         } while (opcionConsulta != 3);
     }
 
     public void consultarPorAutor() {
+        if(contador == 0){
+            System.out.println("No hay instrumentos registrados.");
+            return;
+        }
+
         System.out.print("\nIntroduce el nombre del autor a buscar: ");
         String autorBuscado = leer.nextLine();
         boolean encontrado = false;
 
         System.out.println("\nResultados de Búsqueda");
-        for (Instrumento inst : inventario) {
-            if (inst.getAutor().toLowerCase().contains(autorBuscado.toLowerCase())) {
-                System.out.println(inst.toString());
+        for (int i = 0; i < contador; i++) {
+            if (inventario[i].getAutor().toLowerCase().contains((autorBuscado.toLowerCase()))) {
+                System.out.println(inventario[i].toString());
                 encontrado = true;
             }
         }
-
         if (!encontrado) {
             System.out.println("No se encontraron instrumentos de ese autor.");
         }
     }
 
     public void consultarPorTipo() {
+        if(contador == 0){
+            System.out.println("No hay instrumentos registrados.");
+            return;
+        }
+
         System.out.print("\nIntroduce el tipo, identificar o manejar): ");
         String tipoBuscado = leer.nextLine();
         boolean encontrado = false;
 
-        System.out.println("\n Resultados de Búsqueda");
-        for (Instrumento inst : inventario) {
-            if (inst.getTipo().equalsIgnoreCase(tipoBuscado)) {
-                System.out.println(inst.toString());
+        System.out.println("\nResultados de Búsqueda");
+        for (int i = 0; i < contador; i++) {
+            if (inventario[i].getTipo().equalsIgnoreCase(tipoBuscado)) {
+                System.out.println(inventario[i].toString());
                 encontrado = true;
             }
         }
-
         if (!encontrado) {
             System.out.println("No se encontraron instrumentos de ese tipo.");
         }
     }
+
+    public void aumentarCapacidad(){
+        int capacidadActual = inventario.length;
+        int aumento = (int) (capacidadActual * 0.25);
+        if (aumento == 0){
+            aumento = 1;
+        }
+        int nuevaCapacidad = capacidadActual + aumento;
+        Instrumento[] nuevoArreglo = new Instrumento[nuevaCapacidad];
+
+        for (int i = 0; i < contador; i++){
+            nuevoArreglo[i] = inventario[i];
+        }
+        inventario = nuevoArreglo;
+
+        System.out.println("Se aumento la capacidad en: " + nuevaCapacidad);
+    }
+
+    public String eliminarElemento()
+
 }
